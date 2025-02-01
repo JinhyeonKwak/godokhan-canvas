e7b86f83c0631b13c17c3964809119b59b82f9de
곽진현 <jinhyeon@jinhyeon-mac-pro.local>
2025-02-01 19:15:57 +0900

    라이브러리 버전 최신화

--- server/godokhan-canvas-server/config/db.js
+++ server/godokhan-canvas-server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}`;
+    const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}`;
     await mongoose.connect(mongoURI);
     console.log("[MongoDB] Connected successfully.");
   } catch (error) {
