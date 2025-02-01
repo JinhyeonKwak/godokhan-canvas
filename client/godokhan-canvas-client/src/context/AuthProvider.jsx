f09edf788489c54132e0e93348c9409464c0dbb6
jinhyeon <vmflwu123@gmail.com>
2025-02-01 19:14:41 +0900

    AuthProvider 추가

---
+++ client/godokhan-canvas-client/src/context/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const res = await axios.get(`${API_BASE_URL}/auth/me`, {
+          withCredentials: true,
+        });
+
+        if (res.status === 200) {
+          setIsAuthenticated(true);
+          setUser(res.data.user);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (err) {
+        setIsAuthenticated(false);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, loading, error }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
