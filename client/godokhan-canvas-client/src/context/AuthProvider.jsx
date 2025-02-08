64022930d67fc9f6b77c0227a950c48bdded9fda
곽진현 <vmflwu123@gmail.com>
2025-02-08 23:41:43 +0900

    useAuth 삭제

--- client/godokhan-canvas-client/src/context/AuthProvider.jsx
+++ client/godokhan-canvas-client/src/context/AuthProvider.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const AuthContext = createContext();
+const AuthContext = createContext(undefined);
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
